2633c021ad5ff61f25f5cc7e05ff24a0e4346318
Michael Eskander <michael.eskander.3@gmail.com>
2022-07-06 18:01:28 -0700

    minor error in Sidebar fixed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,4 @@
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
